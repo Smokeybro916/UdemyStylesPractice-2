50ce71a618481da4b7e8e8f116711088e193f818
Smokeybro916 <kyleluangasa@gmail.com>
2023-09-25 19:49:15 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']; //PLEASE DON'T CHANGE THIS LINE!
+
+//YOU CODE GOES HERE:
+const rainbow = document.querySelectorAll('h1 span')
+rainbow[0].style.color = 'red'
+rainbow[1].style.color = 'orange'
+rainbow[2].style.color = 'yellow'
+rainbow[3].style.color = 'green'
+rainbow[4].style.color = 'blue'
+rainbow[5].style.color = 'indigo'
+rainbow[6].style.color = 'violet'
+
+
+/* in here I colored each individual word */
